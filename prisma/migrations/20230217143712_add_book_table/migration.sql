03a68ee517b480976d336c6de00a0f044cb92b16
Julia Hu <juliahukoshi@gmail.com>
2023-02-17 15:41:29 +0100

    initial commit

---
+++ prisma/migrations/20230217143712_add_book_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
